e35af4e32d7538189eff65abcfc9e564a5d07dcc
Jarosław Kuliga <j.kuliga.kontakt@gmail.com>
2016-04-20 16:05:01 +0200

    zaproszenia

---
+++ src/main/java/pl/tarsius/controller/invite/InvitesController.java
@@ -0,0 +1,211 @@
+package pl.tarsius.controller.invite;
+
+import io.datafx.controller.FXMLController;
+import io.datafx.controller.context.ApplicationContext;
+import io.datafx.controller.flow.FlowException;
+import io.datafx.controller.flow.context.ActionHandler;
+import io.datafx.controller.flow.context.FlowActionHandler;
+import io.datafx.controller.util.VetoException;
+import io.datafx.io.DataReader;
+import io.datafx.io.JdbcSource;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.event.WeakEventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.tarsius.controller.BaseController;
+import pl.tarsius.controller.project.ShowProject;
+import pl.tarsius.database.InitializeConnection;
+import pl.tarsius.database.Model.Invite;
+import pl.tarsius.database.Model.Project;
+import pl.tarsius.database.Model.User;
+import pl.tarsius.util.gui.StockButtons;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+
+/**
+ * Created by Ireneusz Kuliga on 19.04.16.
+ */
+@FXMLController(value = "/view/app/myInv.fxml", title = "TaskApp - Moje Zaproszenia")
+public class InvitesController extends BaseController {
+
+    @FXML
+    private ChoiceBox invFiltrSort;
+
+    @FXML
+    private VBox invContent;
+
+    @ActionHandler
+    private FlowActionHandler flowActionHandler;
+    private Logger loger;
+
+    //private ObservableList<Invite> inviteObservableList;
+
+    @PostConstruct
+    public void init() {
+        loger = LoggerFactory.getLogger(getClass());
+        User user = (User) ApplicationContext.getInstance().getRegisteredObject("userSession");
+        new StockButtons(operationButtons, flowActionHandler).homeAction();
+
+        invFiltrSort.setItems(FXCollections.observableArrayList("Sortowanie", "Najnowsze", "Najstarsze"));
+        invFiltrSort.getSelectionModel().selectFirst();
+
+
+
+
+
+        InitializeConnection initializeConnection = new InitializeConnection();
+        try {
+            Task<ObservableList<Invite>> task = inviteTask(null,initializeConnection.connect(),user.getUzytkownikId());
+            new Thread(task).start();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private AnchorPane invRow(Invite invite) {
+        AnchorPane anchorPane = new AnchorPane();
+        try {
+            anchorPane.setPrefWidth(350);
+            anchorPane = FXMLLoader.load(getClass().getClassLoader().getResource("view/app/invitationUserTPL.fxml"));
+            Label projectName = (Label) anchorPane.lookup(".invProjectName");
+            Hyperlink author = (Hyperlink) anchorPane.lookup(".invAuthor");
+            Label endDL = (Label) anchorPane.lookup(".invDescDateEnd");
+            Label endD = (Label) anchorPane.lookup(".invDateEnd");
+            Button cancel = (Button) anchorPane.lookup(".invCancel");
+
+            Button acept = (Button) anchorPane.lookup(".invAccept");
+            Label dateSendL = (Label) anchorPane.lookup(".invDescDateSendLabel");
+            Label dateSend = (Label) anchorPane.lookup(".invDateSend");
+
+
+            cancel.setOnAction(event -> {
+                Button b = (Button) event.getSource();
+                Node n = (Node) b.getParent();
+                Task<Object[]> task = new Task<Object[]>() {
+                    @Override
+                    protected Object[] call() throws Exception {
+                        return Invite.remove(invite.getZaproszeniId());
+                    }
+                };
+                task.setOnRunning(event1 -> loading.setVisible(true));
+                task.setOnSucceeded(event1 -> {
+                    loading.setVisible(false);
+                    if((boolean)task.getValue()[0]) {
+                        try {
+                            flowActionHandler.navigate(InvitesController.class);
+                        } catch (VetoException e) {
+                            loger.debug("Cancdel Inv Task", e);
+                        } catch (FlowException e) {
+                            loger.debug("Cancdel Inv Task", e);
+                        }
+                    } else {
+                        new Alert(Alert.AlertType.INFORMATION,""+task.getValue()[1]).show();
+                    }
+                });
+
+
+                new Thread(task).start();
+            });
+
+            acept.setOnAction(event -> {
+                loading.setVisible(true);
+                    ApplicationContext.getInstance().register("projectId", invite.getProjektId());
+                    Task<Object[]> task = new Task<Object[]>() {
+                        @Override
+                        protected Object[] call() throws Exception {
+                            Object[] resp = Project.addUserToProject(invite.getUzytkownikId(),invite.getProjektId());
+                            if((boolean)resp[0]) {
+                                Object[] ir = Invite.remove(invite.getZaproszeniId());
+                                if ((boolean)ir[0]) resp = ir;
+                            }
+                            return resp;
+                        }
+                    };
+                    task.setOnSucceeded(event1 -> {
+                        try {
+                            if((boolean)task.getValue()[0]) {
+                                flowActionHandler.navigate(ShowProject.class);
+                            }
+                            loading.setVisible(false);
+                            new Alert(Alert.AlertType.ERROR,(String) task.getValue()[1]).show();
+                        } catch (VetoException e) {
+                            e.printStackTrace();
+                        } catch (FlowException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+                    new Thread(task).start();
+
+
+
+            });
+
+            projectName.setText(invite.getNazwaProjektu());
+            author.setText(invite.getUzytkownikImieNazwisko());
+            endD.setText(invite.getDataZakonczenia().toString());
+            dateSend.setText(invite.getDataDodania().toString());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            return anchorPane;
+        }
+
+    }
+
+    public Task<ObservableList<Invite>> inviteTask(String search, Connection connection, long userId) {
+
+        String sql = "select z.uzytkownik_id,z.zaproszenie_id,p.projekt_id,p.nazwa,p.data_zakonczenia,lider,u.imie,u.nazwisko,z.data_dodania " +
+                "from Zaproszenia z,Projekty p,Uzytkownicy u " +
+                "where p.projekt_id=z.projekt_id " +
+                "and p.lider=u.uzytkownik_id " +
+                "and z.uzytkownik_id="+userId+" and stan=1;";
+
+        //if(search!=null) sql+= " and u.imie like '%"+search+"%'";
+
+        DataReader<Invite> dataReader = new JdbcSource<>(connection, sql, Invite.jdbcConverter());
+
+        Task<ObservableList<Invite>> task = new Task<ObservableList<Invite>>() {
+            @Override
+            protected ObservableList<Invite> call() throws InterruptedException, IOException {
+                ObservableList<Invite> observableList = FXCollections.observableArrayList();
+
+                do {
+                    Invite i = dataReader.get();
+                    observableList.add(i);
+                    Thread.sleep(500);
+                } while (dataReader.next());
+                return observableList;
+            }
+        };
+        task.setOnSucceeded(event -> {
+            if(task.getValue().get(0)!=null)
+                task.getValue().forEach(invite -> invContent.getChildren().add(invRow(invite)));
+        });
+        return task;
+    }
+
+
+
+
+
+}
---
+++ src/main/java/pl/tarsius/database/Model/Invite.java
@@ -0,0 +1,171 @@
+package pl.tarsius.database.Model;
+
+import com.mysql.jdbc.PreparedStatement;
+import io.datafx.controller.context.ApplicationContext;
+import io.datafx.io.converter.JdbcConverter;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.tarsius.database.InitializeConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * Created by Jarosław Kuliga on 18.04.16.
+ */
+public class Invite {
+
+    private  long zaproszeniId;
+    private long projektId;
+    private long uzytkownikId;
+    private int stan;
+
+    private String uzytkownikImieNazwisko;
+    private Timestamp dataDodania;
+    private Timestamp dataZakonczenia;
+    private String nazwaProjektu;
+    private long lider;
+
+    private static Logger loger = LoggerFactory.getLogger(Invite.class);
+
+    public Invite(long projektId, long uzytkownikId, int stan, String uzytkownikImieNazwisko) {
+        this.projektId = projektId;
+        this.stan = stan;
+        this.uzytkownikId = uzytkownikId;
+        this.uzytkownikImieNazwisko = uzytkownikImieNazwisko;
+    }
+
+    public Invite(long projektId, String nazwaProjektu, Timestamp dataZakonczenia, Timestamp dataDodania, String uzytkownikImieNazwisko, long uzytkownikId, long zaproszeniId, long lider) {
+        this.projektId = projektId;
+        this.nazwaProjektu = nazwaProjektu;
+        this.dataZakonczenia = dataZakonczenia;
+        this.dataDodania = dataDodania;
+        this.uzytkownikImieNazwisko = uzytkownikImieNazwisko;
+        this.uzytkownikId = uzytkownikId;
+        this.zaproszeniId = zaproszeniId;
+        this.lider = lider;
+    }
+
+    public Object[] save() {
+        try {
+            Connection connection = new InitializeConnection().connect();
+            PreparedStatement preparedStatement= (PreparedStatement) connection.prepareStatement("insert into Zaproszenia (projekt_id,uzytkownik_id,stan) values (?,?,?);");
+            preparedStatement.setLong(1,this.projektId);
+            preparedStatement.setLong(2,this.uzytkownikId);
+            preparedStatement.setInt(3,this.stan);
+            preparedStatement.executeUpdate();
+            return new Object[]{true, "Zaproszenie dodane"};
+        } catch (SQLException e) {
+            return new Object[]{false,"Błąd bazy danych"};
+        }
+    }
+
+    public static Object[] saveList(List<Invite> lu) {
+        try {
+            Connection connection = new InitializeConnection().connect();
+            PreparedStatement preparedStatement= (PreparedStatement) connection.prepareStatement("insert into Zaproszenia (projekt_id,uzytkownik_id,stan) values (?,?,?);");
+            lu.forEach(invite -> {
+                try {
+                    preparedStatement.setLong(1, invite.getProjektId());
+                    preparedStatement.setLong(2, invite.getUzytkownikId());
+                    preparedStatement.setInt(3, invite.getStan());
+                    preparedStatement.addBatch();
+                } catch (SQLException e) {
+                    loger.debug("InvSaveListLoop: ", e);
+                }
+            });
+            int[] x = preparedStatement.executeBatch();
+            String msg = "";
+            for (int i=0;i<x.length;i++) {
+                if(x[i]==0) msg+=lu.get(x[i]).getUzytkownikImieNazwisko()+" nie został zaproszony\n";
+            }
+            if(msg.length()>0) return new Object[]{true, "Nie wszyscy użytkownicy zostali zaproszeni:\n"+msg};
+            return new Object[]{true, "Zaproszenie dodane"};
+        } catch (SQLException e) {
+            loger.debug("svaeInvList:", e);
+            return new Object[]{false,"Błąd bazy danych"};
+        }
+    }
+
+    public static Object[] getUserInv(long userID) {
+        String sql = "select p.projekt_id,p.nazwa,p.data_zakonczenia,lider,u.imie,u.nazwisko,z.data_dodania from Zaproszenia z,Projekty p,Uzytkownicy u where p.projekt_id=z.projekt_id and p.lider=u.uzytkownik_id and z.uzytkownik_id=? and stan=1;";
+        try {
+            Connection connection = new InitializeConnection().connect();
+            PreparedStatement ps = (PreparedStatement) connection.prepareStatement(sql);
+            ps.setLong(1,userID);
+            ResultSet rs = ps.executeQuery();
+            return new Object[]{ true };
+        } catch (SQLException e) {
+            loger.debug("getUserInv", e);
+            return new Object[]{false,"Problem z bazą"};
+        }
+
+    }
+
+    public static JdbcConverter<Invite> jdbcConverter() {
+        return new JdbcConverter<Invite>() {
+            @Override
+            public Invite convertOneRow(ResultSet resultSet) {
+                try {
+                    String im = resultSet.getString("imie")+" "+resultSet.getString("nazwisko");
+                    return new Invite(resultSet.getLong("projekt_id"),resultSet.getString("nazwa"),resultSet.getTimestamp("data_zakonczenia"),resultSet.getTimestamp("data_dodania"),im,resultSet.getLong("uzytkownik_id"),resultSet.getLong("zaproszenie_id"),resultSet.getLong("lider"));
+                } catch (SQLException e) {
+                    loger.debug("Inv jdbcConverter", e);
+                    return null;
+                }
+
+            }
+        };
+    }
+
+    public static Object[] remove(long id) {
+        try {
+            Connection connection = new InitializeConnection().connect();
+            PreparedStatement ps = (PreparedStatement) connection.prepareStatement("delete from Zaproszenia where zaproszenie_id=?");
+            ps.setLong(1,id);
+            ps.executeUpdate();
+            return new Object[] {true, "Zaproszenie usunięte"};
+        } catch (SQLException e) {
+            loger.debug("Usuwanie zaproszenia", e);
+            return new Object[] {false, "Błąd podczas wykonywania zapytania"};
+        }
+    }
+
+
+    public long getProjektId() {
+        return projektId;
+    }
+
+    public long getUzytkownikId() {
+        return uzytkownikId;
+    }
+
+    public int getStan() {
+        return stan;
+    }
+
+    public Timestamp getDataDodania() {
+        return dataDodania;
+    }
+
+    public Timestamp getDataZakonczenia() {
+        return dataZakonczenia;
+    }
+
+    public String getNazwaProjektu() {
+        return nazwaProjektu;
+    }
+
+    public String getUzytkownikImieNazwisko() {
+        return uzytkownikImieNazwisko;
+    }
+
+    public long getZaproszeniId() {
+        return zaproszeniId;
+    }
+}
