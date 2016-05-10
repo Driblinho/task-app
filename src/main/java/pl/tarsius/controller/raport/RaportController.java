1e3bf4608de51038f2a1aa182264392c9833ed1e
Ireneusz Kuliga <irekkuliga@gmail.com>
2016-05-10 14:52:48 +0200

    Kontroller dla raportów

---
+++ src/main/java/pl/tarsius/controller/raport/RaportController.java
@@ -0,0 +1,10 @@
+package pl.tarsius.controller.raport;
+
+import pl.tarsius.controller.BaseController;
+
+/**
+ * Created by ireq on 05.05.16.
+ */
+public class RaportController extends BaseController {
+
+}
