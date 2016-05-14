7684c806e58fe5f1aa9243f99e701ed35076e637
Ireneusz Kuliga <irekkuliga@gmail.com>
2016-05-14 08:17:51 +0200

    Dodano fxml do kontrolera

--- src/main/java/pl/tarsius/controller/raport/RaportController.java
+++ src/main/java/pl/tarsius/controller/raport/RaportController.java
@@ -1,10 +1,12 @@
 package pl.tarsius.controller.raport;
 
+import io.datafx.controller.FXMLController;
 import pl.tarsius.controller.BaseController;
 
 /**
  * Created by ireq on 05.05.16.
  */
+@FXMLController(value = "/view/app/generatingReports.fxml", title = "Generowanie Raportów TaskApp")
 public class RaportController extends BaseController {
 
 }
